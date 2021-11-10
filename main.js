5efb00bea45a7e15c67b8bea889582788bfa352f
Breno Lopes do Carmo <61760048+zTrolly@users.noreply.github.com>
2021-11-10 14:01:37 -0300

    A

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const Input = getElement('.search-input'),
 
 var pokeName, // Nome ou numero passado na caixa de busca
     pokemon, // Responsavel por guardar os dados recebidos da API
-    card; // Responsavel por receber o HTML 
+    card; // Responsavel por receber o HTML
 
 // Build Functions --------------------------------------------
 
@@ -30,6 +30,24 @@ function requestPokeInfo(url, name) {
 
 // Função responsavel por montar o HTML exibido na pagina
 function createCard(){
+  type = pokemon.types.map(item => item.type.name).toString();
+  if (type =="electric"){
+    card = `
+    <div class="card card-electric">
+      <div class="pokemon-picture">
+        <img src="${pokemon.sprites.front_default}">
+      </div>
+      <div class="pokemon-info">
+          <h1 class="name">${pokemon.name}</h1>
+          <h2 class="number">Nº${pokemon.id}</h2>
+          <h3 class="type">Type:${type}</h3>
+          <h3 class="weight">Weight:${pokemon.weight/10}kg</h3>
+          <h3 class="height">Height:${pokemon.height/10}m</h3>
+      </div>
+    </div>`
+    return card
+  }
+  
   card = `
     <div class="card">
       <div class="pokemon-picture">
@@ -37,10 +55,10 @@ function createCard(){
       </div>
       <div class="pokemon-info">
           <h1 class="name">${pokemon.name}</h1>
-          <h2 class="number">Nº ${pokemon.id}</h2>
-          <h3 class="type">Type: ${pokemon.types.map(item => item.type.name).toString()}</h3>
-          <h3 class="weight">Weight: ${pokemon.weight/10}kg</h3>
-          <h3 class="height">Height: ${pokemon.height/10}m</h3>
+          <h2 class="number">Nº${pokemon.id}</h2>
+          <h3 class="type">Type:${type}</h3>
+          <h3 class="weight">Weight:${pokemon.weight/10}kg</h3>
+          <h3 class="height">Height:${pokemon.height/10}m</h3>
       </div>
     </div>`
   return card;
@@ -69,8 +87,5 @@ Button.addEventListener('click', event => {
   startApp(pokeName);
   container.classList.add('fade');
 
-  // Reseta o efeito fade removendo a classe fade
-  setTimeout(() => {
-    container.classList.remove('fade');
-  }, 3000);
+
 });
